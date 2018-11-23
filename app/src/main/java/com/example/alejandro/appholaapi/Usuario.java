8bae042be89825797265ec2b85455a8f7c346a10
732306 <732306@unizar.com>
2018-11-23 18:24:10 +0100

    primer commit

---
+++ app/src/main/java/com/example/alejandro/appholaapi/Usuario.java
@@ -0,0 +1,13 @@
+package com.example.alejandro.appholaapi;
+
+public class Usuario{
+    public String id;
+    public String usuario;
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getId(){
+        return id;
+    }
+}
